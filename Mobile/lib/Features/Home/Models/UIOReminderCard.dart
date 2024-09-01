26b18bc166b8182716c540306d9e45ab6037f71c
Ethan Ivanier <ethanivanier@gmail.com>
2024-09-01 13:38:11 +1200

    create contacts view

--- Mobile/lib/Features/Home/Models/UIOReminderCard.dart
+++ Mobile/lib/Features/Home/Models/UIOReminderCard.dart
@@ -1,13 +1,15 @@
+import 'package:RemindMate/Domain/Database/Models/Contact.dart';
+import 'package:RemindMate/Domain/Database/Models/ReminderType.dart';
 import 'package:RemindMate/Features/Home/Models/UIOReminderCardFriend.dart';
 import 'package:RemindMate/Features/Home/Models/UIOReminderCardType.dart';
 import 'package:intl/intl.dart';
 
 final class UIOReminderCard {
-  String title;
-  UIOReminderCardType cardType;
-  List<UIOReminderCardFriend> friends;
-  DateTime dateTime;
-  bool showTime;
+  late String title;
+  late UIOReminderCardType cardType;
+  late List<UIOReminderCardFriend> friends;
+  late DateTime dateTime;
+  late bool showTime;
 
   UIOReminderCard(
       {required this.title,
@@ -15,9 +17,22 @@ final class UIOReminderCard {
       required this.friends,
       required this.dateTime,
       required this.showTime});
-  
+
+  UIOReminderCard.db(ContactReminder reminder) {
+    this.title = reminder.name!;
+    switch (reminder.reminderType) {
+      case null:
+        this.cardType = UIOReminderCardType.calender;
+      case ReminderType.event:
+        this.cardType = UIOReminderCardType.calender;
+    }
+
+    this.friends = [];
+    this.dateTime = reminder.startTime!;
+    this.showTime = reminder.showTime!;
+  }
+
   String getDateTimeAsString(String format) {
     return DateFormat(format).format(dateTime);
   }
-  
 }
