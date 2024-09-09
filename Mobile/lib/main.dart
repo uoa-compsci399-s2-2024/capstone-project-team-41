3357e2dc8fbe164986526235b0dfbec5f27c3307
ocha091 <ocha091@aucklanduni.ac.nz>
2024-09-09 14:23:56 +1200

    Added firebase to app and cloud-messaging plugin

--- Mobile/lib/main.dart
+++ Mobile/lib/main.dart
@@ -9,11 +9,18 @@ import 'package:RemindMate/Features/Main/MainViewModel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
 import 'Domain/GrpcConnector/ExampleGrpcConnector.dart';
 import 'Features/Main/MainView.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   //Always initialize the grpc connector
   ExampleGrpcConnector().init();
   RemindMateGrpcConnector().init();
