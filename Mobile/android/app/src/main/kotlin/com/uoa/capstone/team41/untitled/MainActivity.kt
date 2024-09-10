b17bdf7e908f5e8509a1818d895227e0cfb4d63b
ocha091 <ocha091@aucklanduni.ac.nz>
2024-09-10 21:02:17 +1200

    Added check to android app to check the device for a compatible Google Play services APK. Added firebase admin sdk to server

--- Mobile/android/app/src/main/kotlin/com/uoa/capstone/team41/untitled/MainActivity.kt
+++ Mobile/android/app/src/main/kotlin/com/uoa/capstone/team41/untitled/MainActivity.kt
@@ -1,5 +1,39 @@
 package com.uoa.capstone.team41.untitled
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 
-class MainActivity: FlutterActivity()
+/* I'm unsure if my Kotlin code is 100% */
+
+class MainActivity: FlutterActivity() {
+
+    private val PLAY_SERVICES_RESOLUTION_REQUEST = 9000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        checkGooglePlayServices()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkGooglePlayServices()
+    }
+
+    private fun checkGooglePlayServices() {
+        val apiAvailability = GoogleApiAvailability.getInstance()
+        val resultCode = apiAvailability.isGooglePlayServicesAvailable(this)
+
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                // prompt the user to download Google Play services.
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)?.show()
+            } else {
+                // Google Play services is not supported on this device
+                // alert the user that the app will not work as intended somehow?
+                // hardly any phones are < android 4.4 so shouldn't have to worry
+            }
+        }
+    }
+}
--- Server/remind-mate/grpc-server/src/main/java/com/capstone/group41/remind/mate/RemindMateApplication.java
+++ Server/remind-mate/grpc-server/src/main/java/com/capstone/group41/remind/mate/RemindMateApplication.java
@@ -1,12 +1,38 @@
 package com.capstone.group41.remind.mate;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @SpringBootApplication
 public class RemindMateApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RemindMateApplication.class, args);
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("Server/remind-mate/grpc-server/src/main/resources/config/firebase-service-acc-credentials.json");
+            // ^ the file in this path is confidential and so has been kept from the repo.
+            // Contact me for a copy, add it to this path and include it in remind-mate/.gitignore
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (FileNotFoundException e) {
+            System.err.println("Firebase service account file not found.");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
