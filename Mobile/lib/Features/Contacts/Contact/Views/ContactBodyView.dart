c3e2fd8d36f59197fcc6dc2090a05e8bc9b81245
tlightfootr <tomlightfootr@gmail.com>
2024-09-14 15:08:04 +1200

    Contact View Styled
    missing add reminder button
    Fields not editable

--- Mobile/lib/Features/Contacts/Contact/Views/ContactBodyView.dart
+++ Mobile/lib/Features/Contacts/Contact/Views/ContactBodyView.dart
@@ -1,6 +1,9 @@
 import 'package:RemindMate/Features/Contacts/Models/UIOContact.dart';
+import 'package:RemindMate/Features/Views/ColorPalette.dart';
 import 'package:RemindMate/Features/Views/TextStyles.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 
 class ContactBodyView extends StatelessWidget {
   final UIOContact uio;
@@ -10,30 +13,89 @@ class ContactBodyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.only(left: 32),
+        padding: const EdgeInsets.only(top: 20, left: 0),
         child: Column(
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Column(
-                  children: [const Text("Relationship Type"), Text(uio.type.title)],
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Relationship Type", style: Textstyles.S1),
+                    const Padding(padding: EdgeInsets.only(bottom: 7),),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      decoration: BoxDecoration(
+                      color: ColorPalette.secondaryPink,
+                      borderRadius: const BorderRadius.all(Radius.circular(16)),
+                      ),
+                      child: Text(uio.type.title)
+                    )
+
+                  ],
                 ),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text("Birthday"),
-                    Row(
-                      children: [Text(uio.birthday)],
+                    const Text("Birthday", style: Textstyles.S1),
+                    const Padding(padding: EdgeInsets.only(bottom: 7),),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: const BorderRadius.all(Radius.circular(16)),
+                      ),
+                      child: Text(DateFormat("dd MMMM").format(DateTime.parse(uio.birthday)), style: TextStyle(fontSize: 15),)
                     )
+                    
                   ],
                 )
               ],
             ),
-            const Text("Phone"),
-            Text(uio.phoneNumber),
-            const Text("Email"),
-            Text(uio.email),
-            const Text("Notes"),
-            Text(uio.notes)
+            Container(
+              padding: EdgeInsets.only(left: 50, right: 50),
+              alignment: Alignment.centerLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(padding: EdgeInsets.only(bottom: 20)),
+                  const Text("Phone", style: Textstyles.boldSmall),
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 7),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: ColorPalette.coolGray,
+                      borderRadius: const BorderRadius.all(Radius.circular(8)),
+                    ),
+                    child: Text(uio.phoneNumber)
+                  ),
+                  const Padding(padding: EdgeInsets.only(bottom: 20)),
+                  const Text("Email", style: Textstyles.boldSmall),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 7),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: ColorPalette.coolGray,
+                      borderRadius: const BorderRadius.all(Radius.circular(8)),
+                    ),
+                    child: Text(uio.email),
+                  ),
+                  const Padding(padding: EdgeInsets.only(bottom: 20)),
+                  const Text("Notes", style: Textstyles.boldSmall),
+                  Container(
+                    padding: EdgeInsets.only(top: 8, bottom: 50, left: 7),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: ColorPalette.coolGray,
+                      borderRadius: const BorderRadius.all(Radius.circular(8)),
+                    ),
+                    child: Text(uio.notes)
+                  ),
+                  const Padding(padding: EdgeInsets.only(bottom: 40)),
+                ],
+              ),
+            )
           ],
         ));
   }
--- Mobile/lib/Features/Views/ColorPalette.dart
+++ Mobile/lib/Features/Views/ColorPalette.dart
@@ -4,8 +4,9 @@ final class ColorPalette {
   static Color primaryGreen = const Color(0xFFD7E6C6);
   static Color secondaryGreen = Color.fromARGB(255, 177, 189, 163);
   static Color primaryPink = const Color(0xFFFF8087);
-  static Color secondaryPink = const Color(0xFFFF7D88);
+  static Color secondaryPink = Color.fromARGB(255, 255, 205, 209);
   static Color primaryOrange = const Color(0xFFFFB366);
   static Color lightGray = const Color(0xFFF4F3F1);
   static Color midGray = const Color(0xFFF1F5F9);
+  static Color coolGray = const Color(0xFFe9edf2);
 }
