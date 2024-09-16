5a817a94528b4a0e9f646ec7740ca512288ed78e
seanallen555 <106068214+seanallen555@users.noreply.github.com>
2024-09-16 16:08:10 +1200

    refactor of fcmservice

--- Server/remind-mate/grpc-server/src/main/java/com/capstone/group41/remind/mate/controller/RemindMateController.java
+++ Server/remind-mate/grpc-server/src/main/java/com/capstone/group41/remind/mate/controller/RemindMateController.java
@@ -2,7 +2,6 @@ package com.capstone.group41.remind.mate.controller;
 
 import com.capstone.group41.remind.mate.service.GetMyDataService;
 import com.capstone.group41.remind.mate.service.UpdateMyDataService;
-import com.capstone.group41.remind.mate.service.UserService;
 import io.grpc.stub.StreamObserver;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,7 +15,6 @@ public class RemindMateController extends RemindMateServiceGrpc.RemindMateServic
 
     private final GetMyDataService getMyDataService;
     private final UpdateMyDataService updateMyDataService;
-    private final UserService userService;
 
     @Override
     public void getMyData(GetMyDataRequest request, StreamObserver<GetMyDataResponse> responseObserver) {
@@ -31,15 +29,4 @@ public class RemindMateController extends RemindMateServiceGrpc.RemindMateServic
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
-
-    @Override
-    public void addFcmToken(AddFcmTokenRequest request, StreamObserver<AddFcmTokenResponse> responseObserver) {
-        var response = userService.addFcmToken(request);
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
-
-    }
-
-
-
 }
